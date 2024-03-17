8f4a35d17db514830ac9e6f990807158111e42a4
ntduc-let <ducnt@proxglobal>
2024-03-17 08:12:30 +0700

    update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.text.SimpleDateFormat
+import java.util.Date
+
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
@@ -5,14 +8,24 @@ plugins {
 
 android {
     namespace = "com.android.jetpack.compose.ntduc.baseproject"
-    compileSdk = 34
+    compileSdk = libs.versions.compileSdkVersion.get().toInt()
 
     defaultConfig {
         applicationId = "com.android.jetpack.compose.ntduc.baseproject"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+
+        minSdk = libs.versions.minSdkVersion.get().toInt()
+        targetSdk = libs.versions.targetSdkVersion.get().toInt()
+
+        val versionMajor = 1
+        val versionMinor = 0
+        val versionPatch = 0
+        versionCode = (versionMajor * 100 + versionMinor * 10 + versionPatch)
+        versionName = "${versionMajor}.${versionMinor}.${versionPatch}"
+
+        val date = SimpleDateFormat("dd.MM.yyyy").format(Date())
+        val hour = SimpleDateFormat("HH").format(Date())
+        val minutes = SimpleDateFormat("mm").format(Date())
+        base.archivesName = "Base_Project_V${versionName}_${date}_${hour}h${minutes}"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -22,7 +35,9 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
@@ -37,7 +52,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.10"
     }
     packaging {
         resources {
